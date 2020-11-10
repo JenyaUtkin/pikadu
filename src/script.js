df2fb6a6366661d5544a99b17d2c27b46beb2640
Evgeniy Utkin <zavalibk@yandex.ru>
2020-11-10 20:07:17 +0300

    finish HTML

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
+'use strict';
 
+const menuToggle = document.querySelector('#menu-toggle');
+const menu = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', function (event) {
+    event.preventDefault();
+    menu.classList.toggle('visible');    
+})
